336b1a4052a600cc0d9cd439eb5a358fb53e1f4e
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-03-09 09:57:15 +0100

    add counter_app_with_selective_rebuild example

---
+++ example/counter_app_with_selective_rebuild/lib/provider.dart
@@ -0,0 +1,18 @@
+// provider.dart
+
+import 'package:flutter/widgets.dart';
+import 'package:reduced_setstate/reduced_fluttertriple.dart';
+
+import 'state.dart';
+
+class MyAppStateProvider extends StatelessWidget {
+  const MyAppStateProvider({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => ReducedProvider(
+        initialState: MyAppState(title: 'reduced_setstate example'),
+        child: child,
+      );
+}
